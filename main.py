1b41628911756652b8f91865250ea31a5b728c99
AndryMorgan-Rage <hei.andry.3@gmail.com>
2025-09-03 09:40:43 +0300

    add the code main.py b and c

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from fastapi import FastAPI
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, status
 from pydantic import BaseModel
-
-from starlette.requests import Request
+from typing import List
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"message":"Ok"}
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristic: Characteristic
+phones: List[Phone] = []
+@app.post("/phone", status_code=status.HTTP_201_CREATED)
+def create_phone(phone: Phone):
+    phones.append(phone)
+    return {"message": "Phone created successfully", "phone": phone}
+@app.post("/phone", status_code=201)
+def create_phone(phone: Phone):
+    phones_db.append(phone)
+    return {"message": "Phone added successfully"}
 
+@app.get("/phones", response_model=List[Phone])
+def get_phones():
+    return phones
